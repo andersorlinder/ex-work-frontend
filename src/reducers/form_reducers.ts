298bd656d7aeb40c101d8c64cbdafe18ca7e8555
Anders <anders_orlinder@hotmail.com>
2025-12-15 01:49:05 +0100

    Fixed eslint

--- src/reducers/form_reducers.ts
+++ src/reducers/form_reducers.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { InputAction, InputType } from "../models/input_models";
 
 const formReducer = (formState: any, inputAction: InputAction) => {
